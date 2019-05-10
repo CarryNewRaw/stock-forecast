8bb867005a0a7d4e51a244626724167e3c7297f8
sheng.li <sheng.li@sjdrivesafe.com>
2019-05-10 18:04:02 +0800

    stockforecast

--- src/main/java/com/ls/stockforecast/web/controller/stockforecast/StockForecastController.java
+++ src/main/java/com/ls/stockforecast/web/controller/stockforecast/StockForecastController.java
@@ -1,8 +1,17 @@
 package com.ls.stockforecast.web.controller.stockforecast;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Created by sheng.li on 2019/5/10.
  */
+@RestController
 public class StockForecastController {
-
+    @GetMapping(value = "/lstest")
+    public String lstest(HttpServletRequest request) {
+        return "ok";
+    }
 }
